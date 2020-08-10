a68db77fedde54c5b6e0b7257ddbbb59abd547a5
iskul <yuriyskul@gmail.com>
2020-08-10 21:38:12 +0300

    Add full replica NestedScrollView.java

--- app/src/main/java/com/example/distortedgeeffect/MainActivity.java
+++ app/src/main/java/com/example/distortedgeeffect/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.distortedgeeffect;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
 
 import android.os.Bundle;
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        NestedScrollView
     }
 }
